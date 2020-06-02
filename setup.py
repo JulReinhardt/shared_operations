43f64c6b46da2e84bf95e9e0b88e5f4d7a5c118b
Juliane Reinhardt <jreinhardt@lbl.gov>
2020-06-02 00:53:48 -0700

    install package

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
-setup(  name = 'shared_operation',
+setup(  name = 'shared operations',
         version = '0.1',
         description = 'Package providing operation for shared use by pystxm (COSMIC/ALS) and xi-cam.',
         author = 'Juliane Reinhardt',
         author_email = 'jreinhardt@lbl.gov',
-        packages = find_packages(include=['shared_operations', 'shared_operations.*']),
+        url = 'https://github.com/JulReinhardt/shared_operations.git',
+        packages = ['correction'],
         python_requires='>=3'
         )
